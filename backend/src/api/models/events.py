566419342117f155d7fa2b0348ffdb782e946f99
Juan Pablo <jp.cano@uniandes.edu.co>
2021-01-25 21:05:38 -0500

    Database Models

---
+++ backend/src/api/models/events.py
@@ -0,0 +1,41 @@
+from ..utils import db
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields, validate
+import enum
+
+class EventCategory(enum.Enum):
+    conference = "conference"
+    seminar = "seminar"
+    congress = "congress"
+    course = "course"
+
+class Event(db.Model):
+    __tablename__ = "events"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    category = db.Column(db.Enum(EventCategory), server_default=EventCategory.conference)
+    begin_date = db.Column(db.DateTime, nullable=False)
+    end_date = db.Column(db.DateTime, nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    virtual = db.Column(db.Boolean, nullable=False, default=True)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+class EventSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = Event
+        sqla_session = db.session
+
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
+    category = fields.String(required=True, validate=validate.OneOf(
+        ["conference", "seminar", "congress", "course"]
+    ))
+    begin_date = fields.String(required=True)
+    end_date = fields.String(required=True)
+    address = fields.String(required=True)
+    virtual = fields.String()
---
+++ backend/src/api/models/users.py
@@ -0,0 +1,42 @@
+from ..utils import db
+import enum
+from passlib.hash import pbkdf2_sha256 as sha256
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
+
+class Role(enum.Enum):
+    admin = "admin"
+    promoter = "promoter"
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    role = db.Column(db.Enum(Role), server_default=Role.promoter)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    is_verified = db.Column(db.Boolean, nullable=False, default=False)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+
+    @staticmethod
+    def verify_hash(password, _hash):
+        return sha256.verify(password, _hash)
+
+class UserSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = User
+        sqla_session = db.session
+
+    id = fields.Integer(dump_only=True)
+    email = fields.Email(required=True)
